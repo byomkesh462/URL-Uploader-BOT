5fe2e3ca26ceb7e3e72de729fe75779b78638dcd
byomkesh462 <88849299+byomkesh462@users.noreply.github.com>
2022-01-15 11:06:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,6 @@ if __name__ == "__main__" :
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
     Warrior = Clinton(
-        "@UPLOADER_X_BOT",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
